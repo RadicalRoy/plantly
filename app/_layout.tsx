782a994806dd8767d49d87c3dfe1e384f1c499b9
radicalroy <roy@thexcompany.com>
2025-02-17 14:07:40 -0800

    implement quick action navigating to new plant modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
 import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -7,6 +11,19 @@ SplashScreen.preventAutoHideAsync();
 */
 
 export default function Layout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
