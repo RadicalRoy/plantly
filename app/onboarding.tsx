a1de2a62380535c8de161f2929f6889246cf3709
radicalroy <roy@thexcompany.com>
2025-02-16 16:06:23 -0800

    implement custom fonts and handle platform specific pathing

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
@@ -50,5 +50,13 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     textAlign: "center",
   },
-  tagline: { fontSize: 24, color: theme.colorWhite, textAlign: "center" },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: "center",
+    fontFamily: Platform.select({
+      ios: "Caveat-Regular",
+      android: "Caveat_400Regular",
+    }),
+  },
 });
